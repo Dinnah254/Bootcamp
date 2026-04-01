aab192c14e2bb9580880d5d6479381dc9e635f8c
Dinnah254 <ezekieldinah46@gmail.com>
2026-04-01 14:03:19 +0300

    my sixth commit

---
+++ javascrpt-tutorial/index.js
@@ -0,0 +1,142 @@
+// // // alert("hellow welcome")
+// // // console.log("hello world")
+// // // let secondName="Mwangi";
+// // // const pi=3.142;
+// // // let isRaining=true;
+// // // let mayage=null;
+// // // let thirdName;
+
+// // // let fruits =["banana","apple","avacado"]
+// // // let album ={"artistName": "Benson Boone","albumName":"Tears",
+// // //   "yearOfPublishing":2014}
+  
+// // // firstName="Mary";
+// // // // Arithmetic operators(+,-,/,*,%,**)
+// // // let a=10;
+// // // let b=20;
+// // // console.log(a+b);
+// // // console.log(a-b);
+// // // console.log(a*b);
+// // // console.log(a/b);
+// // // console.log(a%b);
+// // // console.log(a**b);
+// // // // comparison/relational operators(>,<,>=,<=,==,===)
+// // // //logical operators(&&,||,!)
+
+// // // let num1 =prompt("Enter num1:")
+// // // let num2 =prompt("Enter num2:")
+
+// // button.addEvntListener("click",function(){
+// //   let num1 =document.getElementsById
+// //   ("num1").value;
+// //   let num2 =document.getElementsById("num2").value;
+
+
+// // let result = Number(num1)+Number(num2);
+// // console.log("your result is:" ,result);
+// // let output= document.getElementById("results");
+// // output.innerText=` your result is: ${"result"}`
+// // })
+
+// // // console.log(typeof(secondName));
+// // // console.log(typeof(isRaining));
+// // // console.log(typeof(mayage));
+// // // console.log(typeof(fruits));
+// // // console.log(typeof(album));
+// // // console.log(typeof(thirdName));
+
+
+
+
+
+
+// // let marks =50;
+// // if(marks>=70 && marks<100){
+// //   console.log("grade"=B)
+// // }
+// // else if(marks>=60 && marks<70){
+// //   console .log()
+
+// // }
+// // else if(marks>)
+
+// for(let i=1;i<21;i++){
+          
+//           console.log(i)
+// }
+// let counter=20;
+
+// while(counter<30){
+//       counter++;
+//       console.log("loops are amaizing")
+// }
+
+// do{
+
+//     console.log("i am happy")
+//     counter++;
+// }
+// while()
+   
+//   fruits=["Apple","banana", "mangoes"]
+//    for(let fruits of fruits) 
+//     {
+//         console.log(fuits)
+//     }  
+
+//     function  addNumber(a,b){
+//       let sum=a+b;
+//       console.log(sum)
+//     }
+
+
+
+
+
+
+// function  addNumber(a,b){
+
+//   return a+b;
+// }
+// console.log(addNumbers(8));
+
+// // function expression
+// const subNumbsrs =(a,b){return a-b}
+
+// console.log(subNumbers(40,10))
+
+// // arrow functions
+// const divNumbers = (a,b){return a/b;}
+// console.log(divsubNumbers(40,10))
+
+
+// let counter=0;
+// if(counter<10)
+// {
+//   let age =30;
+//   console .log(`my age is $ (age)`)
+// }
+//  console .log(`my age is $ (age)`)
+
+
+//  let el=document.querySelectorAll("P")
+//  console.log(el)
+//  el.textContent="hellow world"
+
+//  el.innerHTML=`<p>hello. world`
+
+
+
+
+
+
+let el2=document.querySelector("div");
+
+const p =document.createElement("p")
+let paragraph =`<p>Hello this a paragraph from javascript </p>`
+p.innerText=paragraph;
+document.body.appendChild(paragraph)
+
+let el4=document.querySelector("button")
+
+el4.addEventListener()
