f52c55d441454c48eff2ce6082d127e83ab02c69
Dinnah254 <ezekieldinah46@gmail.com>
2026-04-09 14:06:24 +0300

    api tutorial

---
+++ Dinnah_LTC/dom- exercise/data-structure/index.js
@@ -0,0 +1,17 @@
+let fruits = ["apple","mango","pineapple"];
+
+    console.log(fruits[0])
+
+    fruits[1] = "Avocado";
+    console.log(fruits)
+
+    fruits.push("banana")
+
+    console.log(fruits)
+    fruits.unshift("pears")
+    console.log(fruits)
+
+    fruits.pop()
+    console.log(fruits)
+    fruits.shift()
+    console.log(fruits)
